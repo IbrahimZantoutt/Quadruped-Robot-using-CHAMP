8b909d03d85c251bde9eba60c7dcce0cc7f2f2ea
Ibrahim Zantout <kingofdeath3636@gmail.com>
2026-06-23 17:20:09 +0300

    the project launches on gazebo perfectly, no physics problems like before champ

---
+++ build/champ_base/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ibrahim/ChampQuadbot/install/champ/share/champ/package.sh"
+. "/home/ibrahim/ChampQuadbot/install/champ_msgs/share/champ_msgs/package.sh"
